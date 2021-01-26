d2bedd1263fd49a35b6fd7ceb3ac9a26fab5ef80
JubaerHossain <jubaer01.cse@gmail.com>
2021-01-26 18:22:36 +0600

    multi auth

--- server.js
+++ server.js
@@ -13,26 +13,27 @@ const environment = process.env.NODE_ENV;
 const enVal = process.env;
 const stage = require('./config')[environment];
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 if (environment != 'production') {
     app.use(logger('dev'));
 }
-async () => {
     try {
-        await mongoose.connect( enVal.DB_HOST, {
+         mongoose.connect(enVal.DB_HOST, {
             useNewUrlParser: true,
-            useCreateIndex: true,
             useUnifiedTopology: true,
-            useFindAndModify: false
-        });
+            useFindAndModify: false,
+            useCreateIndex: true
+          });
         console.log("MongoDB Conected")
     } catch (err) {
+        console.error('err.message');
         console.error(err.message);
         process.exit(1);
     }
-};
+
+
 app.get('/', function(req, res) {
     res.send('hello node.js');
 });
